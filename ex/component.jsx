0331a13d898b743bfa6e99ad606b1be8245722a3
Rodrigo Bessa <rbessa_analista@hotmail.com>
2019-02-14 00:24:15 -0200

    Função lambda

--- ex/component.jsx
+++ ex/component.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
-export default function () {
-    return <h1>Primeiro Componente 2!</h1>
-}
+// export default function () {
+//     return <h1>Primeiro Componente 2!</h1>
+// }
+
+export default () => (<h1>Primeiro Componente 2!</h1>)
